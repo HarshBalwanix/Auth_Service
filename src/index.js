2f7484e7699aadc485d677429970d5b510d108de
Harsh Balwani <balwaniharsh1001@gmail.com>
2023-04-13 22:43:47 +0530

    Signup Logic with password encrytion done

--- src/index.js
+++ src/index.js
@@ -1,10 +1,18 @@
 const express=require("express");
+const bodyParser=require('body-parser');
 
 const {PORT}=require('./config/serverconfig');
+const apiRoutes=require('./routes/index');
 
 const app =express();
 
 const prepareAndStartServer =() =>{
+    
+    app.use(bodyParser.json());
+    app.use(bodyParser.urlencoded({extended:true}));
+
+    app.use('/api',apiRoutes);
+
     app.listen(3001,()=>{
         console.log('server started on PORT',PORT);
     });
